1e22d0e567f3461a5ed39fdfd66175d5a9f2283d
evan-daniel <evan@evandaniel.com>
2021-05-17 00:52:19 -0500

    ten

--- tens/main.js
+++ tens/main.js
@@ -1,106 +1,73 @@
-window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
-    
-    // INIT
-    const h = window.innerHeight * 0.01; 
-    document.documentElement.style.setProperty('--vh', `${h}px`); 
+window.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01 + 'px'); 
+
     const beads = document.querySelectorAll('.beads > div'); 
     const bead_tops = []; 
     let count = 500; 
+    const h = document.querySelector('.bar__vertical').getBoundingClientRect().height; 
 
     let add = +pi.charAt(count); 
-    beads.forEach((bead, bead_index) => {
+    beads.forEach((bead, beadIndex) => {
         let mousedown = false; 
         let drag_start = 0; 
-        bead_tops[bead_index] = bead.getBoundingClientRect().top; 
-        let bead_start = bead_tops[bead_index]; 
+        bead_tops[beadIndex] = bead.getBoundingClientRect().top; 
+        let bead_start = bead_tops[beadIndex]; 
         const bead_height = bead.getBoundingClientRect().height; 
         let touch_identifier = -1; 
     
-        bead.addEventListener('mousedown', mousedown => {
+        bead.addEventListener('mousedown', beadMousedown => {
             mousedown = true; 
-            drag_start = mousedown.y; 
-            bead_start = bead_tops[bead_index]; 
+            drag_start = beadMousedown.y; 
+            bead_start = bead_tops[beadIndex]; 
         }); 
-        bead.addEventListener('touchstart', touchstart => {
-            touchstart.preventDefault(); 
+        bead.addEventListener('touchstart', beadTouchstart => {
+            beadTouchstart.preventDefault(); 
             mousedown = true; 
-            touch_identifier = touchstart.which; 
-            drag_start = touchstart.targetTouches[0].clientY; 
-            bead_start = bead_tops[bead_index]; 
+            touch_identifier = beadTouchstart.which; 
+            drag_start = beadTouchstart.targetTouches[0].clientY; 
+            bead_start = bead_tops[beadIndex]; 
         }); 
 
         const lift = () => {
             let number = 0; 
-            for(let i = 0; i < 10; i++) {
-                if(bead_tops[i] < i * bead_height + 0.5 * bead_height) {
-                    ++number; 
-                }
-            }
+            if(h * 0.07 < bead_tops[0]) number = 5; 
+            if(bead_tops[1] < h * 0.37) number += 1; 
+            if(bead_tops[2] < h * 0.51) number += 1; 
+            if(bead_tops[3] < h * 0.65) number += 1; 
+            if(bead_tops[4] < h * 0.79) number += 1; 
 
             if(number === add) {
                 add = (add + +pi.charAt(++count)) % 10; 
             }
             document.querySelector('.count').innerHTML = count; 
-            document.querySelector('.entered').innerText = number; 
         }; 
-        bead.addEventListener('mouseup', () => {
+        document.addEventListener('mouseup', () => {
             mousedown = false; 
             lift(); 
         }); 
-        bead.addEventListener('touchend', touchend => {
-            if(touchend.touches.length === 0) {
+        document.addEventListener('touchend', documentTouchend => {
+            if(documentTouchend.targetTouches.length === 0) {
                 mousedown = false; 
                 lift(); 
             }
         }); 
 
         const move = y => {
-
-            // ONLY RUN WHILE TOUCHING
-            if(!mousedown) {
-                return; 
-            }
+            if(!mousedown) return; 
             
-            // RECORD MOVEMENT
             const delta = y - drag_start; 
-            bead_tops[bead_index] = bead_start - drag_start + y; 
-            
-            // MOVE ALL BEADS TO BE IN-BOUNDS
-            // INCLUDES JUST-MOVED BEAD
+            bead_tops[beadIndex] = bead_start - drag_start + y; 
+    
             for(let i = 0; i < beads.length; i++) {
-                if(bead_tops[i] < i * bead_height) {
-                    bead_tops[i] = i * bead_height; 
-                } 
-                if(i * bead_height + bead_height < bead_tops[i]) {
-                    bead_tops[i] = i * bead_height + bead_height; 
-                }
-            }
-
-            // PROPAGATE MOVEMENT NORTH
-            if(delta < 0) {
-                for(let i = bead_index - 1; -1 < i; i--) {
-                    if(bead_tops[i + 1] - bead_height < bead_tops[i]) {
-                        bead_tops[i] = bead_tops[i + 1] - bead_height; 
-                    } else {
-                        break; 
-                    }
-                }
-            }
-
-            // PROPAGATE MOVEMENT SOUTH
-            if(0 < delta) {
-                for(let i = bead_index + 1; i < beads.length; i++) {
-                    if(bead_tops[i] < bead_tops[i - 1] + bead_height) {
-                        bead_tops[i] = bead_tops[i - 1] + bead_height; 
-                    } else {
-                        break; 
-                    }
-                }
-            }
-
-            // APPLY STYLE
-            for(let i = 0; i < beads.length; i++) {
-                beads[i].style.top = `${bead_tops[i]}px`; 
+                const index = delta < 0 ? beads.length - 1 - i : i; 
+                
+                if(index && delta < 0) bead_tops[index - 1] = Math.min(bead_tops[index - 1], bead_tops[index] - bead_height); 
+                else if(index < 4) bead_tops[index + 1] = Math.max(bead_tops[index] + bead_height, bead_tops[index + 1]); 
+    
+                if(index === 0) bead_tops[index] = Math.max(0, Math.min(bead_height, bead_tops[index])); 
+                else bead_tops[index] = Math.max(h * 0.02 + bead_height * (index + 1), Math.min(h * 0.02 + bead_height * (index + 2), bead_tops[index])); 
+                
+                beads[index].style.top = bead_tops[index] + 'px'; 
             }
         }; 
         bead.addEventListener('mousemove', beadMousemove => move(beadMousemove.y)); 
