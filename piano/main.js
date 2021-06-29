6aab9b7598e28acf036e88d11ee778c3395afa93
evan-daniel <evan@evandaniel.com>
2021-06-29 12:50:13 -0500

    legacy js animation

--- piano/main.js
+++ piano/main.js
@@ -14,14 +14,28 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
     document.querySelector('.piano-container').addEventListener('touchstart', touchstart => {
         if(touchstart.target.classList.contains('key')) {
-            touchstart.target.animate([{
-                backgroundColor: touchstart.target.getAttribute('value') === pi_digits[pi_cursor] && ++pi_cursor ? '#00F' : '#F00', 
-            }, {
-                backgroundColor: touchstart.target.classList.contains('white') ? '#FFF' : '#000', 
-            }], {
-                duration: 500, 
-            }); 
-            // touchstart.target.getAttribute('value') === pi_digits[pi_cursor] && ++pi_cursor
+
+            // MY IPHONE USES TOO OLD AN IOS VERSION TO SUPPORT ANIMATION API
+
+            // CURRENT JS VERSION
+            // touchstart.target.animate([{
+            //     backgroundColor: touchstart.target.getAttribute('value') === pi_digits[pi_cursor] && ++pi_cursor ? '#00F' : '#F00', 
+            // }, {
+            //     backgroundColor: touchstart.target.classList.contains('white') ? '#FFF' : '#000', 
+            // }], {
+            //     duration: 500, 
+            // }); 
+
+            // LEGACY SUPPORT VERSION
+            let pi_entry_was_correct = false; 
+            if(touchstart.target.getAttribute('value') === pi_digits[pi_cursor]) {
+                ++pi_cursor; 
+                pi_entry_was_correct = true; 
+            }
+            touchstart.target.style.animation = ''; 
+            touchstart.target.offsetHeight; 
+            touchstart.target.style.animation = `${touchstart.target.classList.contains('white') ? pi_entry_was_correct ? 'correct-pi-entry-white' : 'mistake-pi-entry-white' : pi_entry_was_correct ? 'correct-pi-entry-black' : 'mistake-pi-entry-black'} 500ms`; 
+
             document.querySelector('.pi-cursor').innerText = pi_cursor; 
         }
     }); 
